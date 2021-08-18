f2a7859cad630f6d8f166d94ae6636dd0909b1d9
Xixo99 <549845545@qq.com>
2021-08-18 11:33:19 +0800

    使用client.py（连续发送10条udp数据）测试时没有问题。自带udp发送存在卡顿

---
+++ client.py
@@ -0,0 +1,29 @@
+from os import read
+import socket
+import sys
+import scipy
+import numpy as np
+import sys
+import time
+
+server_ip = '127.0.0.1'
+server_port = 9999
+
+# rowdata = np.random.randint(0, 255, 200)
+# np.save("row_random_data200.npy", rowdata)
+
+rowdata = np.load("row_random_data200.npy")  # 随机生成的向量，用于模拟含有语义的信号
+noise_db = 50
+# socket.SOCK_DGRAM代表是UDP通信
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect((server_ip, server_port))
+
+for i in range(10):
+    noise = np.random.randint(0, noise_db, 200) - noise_db//2
+    data = rowdata+noise
+    str_ = ' '.join(str(x) for x in data)
+    s.send(str_.encode())
+
+    print("数据发送成功！")
+    time.sleep(0.5)
+s.close()
